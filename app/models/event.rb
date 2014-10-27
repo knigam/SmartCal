aaf6a754c616db0e33b32296ee8e3c1994f87e5e
knigam <kushal.nigam1@gmail.com>
2014-10-27 14:39:52 -0400

    fixes to event delete and conflit resolution

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ActiveRecord::Base
         conflicts = Event.where("start_time < ? AND end_time > ?", self.end_time, self.start_time).order("end_time ASC")
         last = conflicts.last
         if last
-            buffer = (last.end_time - self.end_time).abs + 600
+            buffer = (last.end_time - self.start_time).abs + 600
             self.start_time += buffer
             self.end_time += buffer
             self.check_conflict
