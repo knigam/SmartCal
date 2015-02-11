8c26d5c855aab4749e9d2f91a52164396bab8679
ewang1218 <eileen.wang.1218@gmail.com>
2015-02-10 23:04:39 -0500

    revised application_controller code, acidentally changed some parts, added date-picker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_devise_params, if: :devise_controller?
 
-  def configure_devise_params
+def configure_devise_params
     devise_parameter_sanitizer.for(:sign_up) do |u|
-     u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :phone_no, :email_notifications, :text_notifications)
+      u.permit(:name, :email, :password, :password_confirmation)
     end
   end
 end
