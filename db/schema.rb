d4581188f87cb24f538273a3a950b3621a34faed
Kushal <kushal.nigam1@gmail.com>
2015-04-01 12:08:50 -0400

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,6 @@ ActiveRecord::Schema.define(version: 20150331043800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "events", force: true do |t|
     t.string   "title"
     t.text     "description"
