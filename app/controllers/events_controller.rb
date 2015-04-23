1bf22aecae7c589e46f666c39647dd4eabd84bd1
Kushal <kushal.nigam1@gmail.com>
2015-04-23 03:35:05 -0400

    added home page with welcome controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,5 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
 
   # GET /events
   # GET /events.json
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!
+  def index
+  end
+
+  def about
+  end
+end
